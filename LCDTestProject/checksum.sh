510763f673181addc3fbea144c3761300cf29bc5
beke.andras.mkolc@gmail.com <beke.andras.mkolc@gmail.com@48ed2cb9-36dd-f24f-4ce1-c7c12b04649e>
2012-03-06 10:42:49 +0000

    delete

--- LCDTestProject/checksum.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-hexmate -CK=0003-1FFF@2000w2 dist/default/production/MASZAT-II.production.hex -LOGFILE=output.log -OMASZATCRC.HEX -ADDRESSING=2 -FILL=0000@0003-1FFF
-#hexmate -LOGFILE=output.log
